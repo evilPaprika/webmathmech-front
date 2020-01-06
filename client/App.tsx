7f9f59701b00e01b3823b469700704c6bf7a7c9c
evilPaprika <ivil.paprika@gmail.com>
2020-01-06 19:04:11 +0500

    test-ci-17

--- client/App.tsx
+++ client/App.tsx
@@ -7,11 +7,7 @@ const App: React.FC = () => (
         <header className="App-header">
             {/* <img src={logo} className="App-logo" alt="logo" /> */}
             <p>
-                Edit
-                {' '}
-                <code>src/App.tsx</code>
-                {' '}
-                and save to reload.
+                Hello CD!
             </p>
             <a
                 className="App-link"
