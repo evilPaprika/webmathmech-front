1560b9a5009d5445df2c056f64aa941f158911b5
evilPaprika <ivil.paprika@gmail.com>
2020-01-07 00:03:06 +0500

    test-ci-37

--- client/App.tsx
+++ client/App.tsx
@@ -6,17 +6,10 @@ const App: React.FC = () => (
     <div className="App">
         <header className="App-header">
             {/* <img src={logo} className="App-logo" alt="logo" /> */}
+            <img src="https://www.meme-arsenal.com/memes/b402fcdafad2034c1cdd9038d6f49a16.jpg" alt="yaml development" />
             <p>
                 Hello CD!
             </p>
-            <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                Learn React
-            </a>
         </header>
     </div>
 );
