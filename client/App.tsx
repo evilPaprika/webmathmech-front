d73de18097a2271cdc44bc024210ba184f23d49f
evilPaprika <ivil.paprika@gmail.com>
2020-01-26 16:48:36 +0500

    test

--- client/App.tsx
+++ client/App.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
-// import logo from './logo.svg';
 import './App.css';
 
 const App: React.FC = () => (
     <div className="App">
         <header className="App-header">
-            {/* <img src={logo} className="App-logo" alt="logo" /> */}
             <img src="https://www.meme-arsenal.com/memes/b402fcdafad2034c1cdd9038d6f49a16.jpg" alt="yaml development" />
             <p>
-                Hello CD!
+                WEBMATHMECH!
             </p>
         </header>
     </div>
