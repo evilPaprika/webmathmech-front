da440694fe909a9563d71fbf4e704aedbdea9dd0
Dmitry <dmitry-zel@yandex-team.ru>
2020-03-04 19:32:41 +0500

    эксперименты с показом видео

--- client/components/pages/news-page/index.tsx
+++ client/components/pages/news-page/index.tsx
@@ -1,7 +1,32 @@
 import React, { memo } from 'react';
+import ReactPlayer from 'react-player';
 
+import { Card } from '@material-ui/core';
 import { useStyles } from './styles';
 
+
+interface CardProps {
+    header: React.ReactNode;
+    content: React.ReactNode;
+}
+
+const CARDS: Array<CardProps> = [
+    {
+        header: 'Header',
+        content: `abracadabra asofasklfsdl;fksdl;fkls;dfgkl;dsfgksdfksdfslfsdfsdfsdfsd,fsdlvs,vsdlvsdvsv,sdlv;mdfbv
+        вамваыи
+        ваыив
+        аи
+        ваыивва
+        ив
+        аива
+        ива
+        ивваи
+        аиваив
+        fsb
+        dfbfdb`
+    }
+];
 
 const NewsPage = () => {
     const styles = useStyles();
@@ -17,6 +42,26 @@ const NewsPage = () => {
                 <p>
                     Site is in progress!
                 </p>
+                {CARDS.map(() => (
+                    <Card>
+                        {/* <CardHeader>
+                            {header}
+                        </CardHeader>
+                        <CardContent>
+                            {content}
+                        </CardContent> */}
+                        <ReactPlayer url="https://www.youtube.com/watch?v=_Ht9woqhWmY" playing controls />
+                        {/* <iframe
+                            width="560"
+                            height="315"
+                            title="hello"
+                            src="https://www.youtube.com/watch?v=_Ht9woqhWmY"
+                            frameBorder="0"
+                            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                            allowFullScreen
+                        /> */}
+                    </Card>
+                ))}
             </div>
         </main>
     );
--- client/components/pages/news-page/styles.ts
+++ client/components/pages/news-page/styles.ts
@@ -10,11 +10,10 @@ export const useStyles = makeStyles(() => ({
         boxShadow: '-10px -10px 30px 4px rgba(256,256,256,0.7), 10px 10px 30px 4px rgba(0,0,0,0.5)'
     },
     newsPage__main: {
-        minHeight: '100vh',
         display: 'flex',
         flexDirection: 'column',
         alignItems: 'center',
         justifyContent: 'center',
-        fontSize: 'calc(10px + 2vmin)',
+        height: '100vmin',
     },
 }));
