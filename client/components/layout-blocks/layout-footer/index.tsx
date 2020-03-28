33fb744ab421af333f74c98ff2b131c91f472e72
Ilinykh Ivan <ivil.paprika@gmail.com>
2020-03-28 16:50:30 +0500

    fix typo

--- client/components/layout-blocks/layout-footer/index.tsx
+++ client/components/layout-blocks/layout-footer/index.tsx
@@ -3,7 +3,7 @@ import { Box, Container, Link, Typography } from '@material-ui/core';
 
 import LogoVK from './logos/vk_logo.svg';
 import LogoMail from './logos/mail_logo.svg';
-import LogoGinhub from './logos/github_logo.svg';
+import LogoGithub from './logos/github_logo.svg';
 import { useStyles } from './styles';
 
 
@@ -18,7 +18,7 @@ const FOOTER_LOGOTYPES = [
         href: 'mailto:webmathmech@yandex.ru'
     },
     {
-        SvgSocialIcon: LogoGinhub,
+        SvgSocialIcon: LogoGithub,
         href: 'https://github.com/evilPaprika/webmathmech-front'
     }
 ];
