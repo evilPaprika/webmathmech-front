baaf3a1fe6a2832671b256d4b7d527635c4e0442
Dmitry <dmitry-zel@yandex-team.ru>
2020-04-04 21:36:47 +0500

    добавил разделители

--- client/components/layout-blocks/layout-header/index.tsx
+++ client/components/layout-blocks/layout-header/index.tsx
@@ -1,10 +1,10 @@
 import React, { memo, useCallback, useMemo, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import { AppBar, Container, Drawer, Hidden, SwipeableDrawer, Tab, Tabs, Toolbar } from '@material-ui/core';
+import { AppBar, Container, Divider, Drawer, Hidden, SwipeableDrawer, Tab, Tabs, Toolbar } from '@material-ui/core';
 import { useQuery } from '@apollo/react-hooks';
 
 import { GET_IS_LOGGED_IN } from 'client/apollo/queries';
-import { EXTENDED_HEADER_TABS, HEADER_TABS, ROUTES } from 'client/consts';
+import { EXTENDED_HEADER_TABS, HEADER_TABS, MENU_ITEMS, ROUTES } from 'client/consts';
 import { findMenuItemByPath } from 'client/utils';
 import LayoutFooter from '../layout-footer';
 import { AuthButtons } from './auth-buttons';
@@ -42,11 +42,11 @@ const LayoutHeader = () => {
         setMobileOpen(false);
     }, []);
 
-    const handleDrawerToggle = () => {
+    const onDrawerToggle = () => {
         setMobileOpen(!mobileOpen);
     };
 
-    const Icons = <HeaderIcons onClickByMenu={handleDrawerToggle} onClickByLogo={onClickByLogo} />;
+    const Icons = <HeaderIcons onClickByMenu={onDrawerToggle} onClickByLogo={onClickByLogo} />;
 
     const drawer = (
         <>
@@ -55,15 +55,14 @@ const LayoutHeader = () => {
                     {Icons}
                 </Container>
                 <Tabs value={tab} onChange={onChangeTab} orientation="vertical">
-                    {availableHeaderTabs.map(({ text, path }) => (
-                        <Tab
-                            key={text}
-                            label={text}
-                            value={path}
-                            to={path}
-                            component={Link}
-                        />
+                    {isLoggedIn && MENU_ITEMS.map(({ text, path }) => (
+                        <Tab key={text} label={text} value={path} to={path} component={Link} />
                     ))}
+                    <Divider light />
+                    {HEADER_TABS.map(({ text, path }) => (
+                        <Tab key={text} label={text} value={path} to={path} component={Link} />
+                    ))}
+                    <Divider light />
                 </Tabs>
             </div>
             <LayoutFooter />
@@ -84,8 +83,8 @@ const LayoutHeader = () => {
                         classes={{ paper: styles.drawerPaper }}
                         variant="temporary"
                         open={mobileOpen}
-                        onOpen={handleDrawerToggle}
-                        onClose={handleDrawerToggle}
+                        onOpen={onDrawerToggle}
+                        onClose={onDrawerToggle}
                         ModalProps={{ keepMounted: true }}
                     >
                         {drawer}
