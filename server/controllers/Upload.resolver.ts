6d7c1db5164cef99ae4b1a1a8fa58aa181cf5605
evilPaprika <ivil.paprika@gmail.com>
2020-04-21 18:13:15 +0500

    added image minification

--- server/controllers/Upload.resolver.ts
+++ server/controllers/Upload.resolver.ts
@@ -1,6 +1,10 @@
 import { Resolver, Mutation, Arg } from 'type-graphql';
 import { GraphQLUpload } from 'apollo-server-koa';
 import { ReadStream } from 'fs';
+import sharp from 'sharp';
+import { streamToBuffer } from '@jorgeferrero/stream-to-buffer';
+import path from 'path';
+
 import { minioClient } from '../minio';
 
 
@@ -15,8 +19,20 @@ export interface Upload {
 export class UploadResolver {
     @Mutation(() => String)
     public async fileUpload(@Arg('file', () => GraphQLUpload!) { createReadStream, filename }: Upload) {
+        const { name, ext } = path.parse(filename);
+
+        let fileData: Buffer | ReadStream = createReadStream();
+        if (['.png', '.jpg', '.jpeg'].includes(ext)) {
+            const buffer = await streamToBuffer(fileData);
+            fileData = await sharp(buffer)
+                .jpeg({ quality: 80 })
+                .toBuffer();
+
+            filename = `${name}.jpg`;
+        }
+
         await new Promise((resolve) => {
-            minioClient.putObject('main', filename, createReadStream(), (error: Error | null) => {
+            minioClient.putObject('main', filename, fileData, (error: Error | null) => {
                 if (error) {
                     throw new Error(error.message);
                 }
