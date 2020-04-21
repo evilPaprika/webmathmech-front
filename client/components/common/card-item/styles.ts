de654e2e64aec6b1d786b10c9e09805c4e1c3c92
evilPaprika <ivil.paprika@gmail.com>
2020-04-21 13:33:07 +0500

    card bigger margins

--- client/components/common/card-item/styles.ts
+++ client/components/common/card-item/styles.ts
@@ -3,13 +3,14 @@ import { makeStyles } from '@material-ui/core/styles';
 
 export const useStyles = makeStyles((theme) => ({
     card: {
-        margin: `${theme.spacing(4)}px 0`,
+        margin: `${theme.spacing(4)}px ${theme.spacing(3)}px`,
         maxWidth: 800,
 
         [theme.breakpoints.up('md')]: {
             minWidth: 500,
         },
         [theme.breakpoints.down('sm')]: {
+            margin: `${theme.spacing(4)}px 0`,
             minWidth: 300
         },
     },
