2ddcaa568cf6a610547caaa730699b8198f3dfe3
Dmitry <dmitry-zel@yandex-team.ru>
2020-06-05 01:14:05 +0500

    теперь модалка скроллится, контент снаружи не скроллится

--- client/components/common/modal/styles.ts
+++ client/components/common/modal/styles.ts
@@ -2,23 +2,8 @@ import { makeStyles } from '@material-ui/core/styles';
 
 
 export const useStyles = makeStyles((theme) => ({
-    modal: {
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-
-    modalForm: {
-        maxWidth: '600px',
-
-        backgroundColor: theme.palette.background.paper,
-        borderRadius: '5px',
-
-        overflow: 'hidden',
-        outline: 'none',
-    },
-
-    close: {
+    header: {
         color: theme.palette.primary.contrastText,
-    },
+        background: theme.palette.primary.main
+    }
 }));
