8d9198de88ec0a2994567e653d5176f92d717cff
Dmitry <dmitry-zel@yandex-team.ru>
2020-06-13 14:32:31 +0500

    починил отображение карточек при экране 320px. Убрал отступы для экранов до 600px

--- client/components/common/modal/styles.ts
+++ client/components/common/modal/styles.ts
@@ -5,5 +5,20 @@ export const useStyles = makeStyles((theme) => ({
     header: {
         color: theme.palette.primary.contrastText,
         background: theme.palette.primary.main
+    },
+
+    modalPaper: {
+        [theme.breakpoints.down('xs')]: {
+            margin: 0,
+            // класс .MuiDialog-paperWidthMd.MuiDialog-paperScrollBody перебивает этот стиль
+            maxWidth: 'calc(100% - 5px) !important',
+            width: 'calc(100% - 5px)'
+        }
+    },
+
+    content: {
+        [theme.breakpoints.down('xs')]: {
+            padding: theme.spacing(1)
+        }
     }
 }));
